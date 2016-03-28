e87fc92fbbab0acff143765fbb3d3fde7a518fac
Michał Woźniczak <michal.wozniczak@gmail.com>
2016-03-28 20:15:25 +0200

    Added documentation + made it a bit configurable

--- settings.py
+++ settings.py
@@ -1,2 +1,18 @@
-DEFAULT_SUBSCRIBE_ADDRESS = "tcp://localhost:5677"
+"""Settings for the application:
+
+DEFAULT_SUBSCRIBE_ADDRESS: String.
+                           ZeroMQ address to subscribe to, unless overriden in commandline.
+DEFAULT_SUBSCRIBE_ADDRESS: String.
+                           ZeroMQ address to publish to (used in the example).
+REBROADCAST_INTERVAL: Dict containing datetime.timedelta-compatible params.
+                      Currently not in use, aim was to enable Observables to periodically rebroadcast their current
+                      value
+WEBSOCKETS_ADDRESS: Tuple containing address and port from which to broadcast to websockets.
+"""
+
+
+DEFAULT_SUBSCRIBE_ADDRESS = 'tcp://localhost:5677'
+DEFAULT_PUBLISH_ADDRESS = "tcp://*:5677"
 REBROADCAST_INTERVAL = {'seconds': 5}
+
+WEBSOCKETS_ADDRESS = ('0.0.0.0', 9000)
--- websockets_server.py
+++ websockets_server.py
@@ -1,9 +1,23 @@
+""" Websocket server for graph data pulled from ZeroMQ
+
+Usage: websocket_server.py [ZeroMQ address] [-h|--help]
+
+    [ZeroMQ address] - ZeroMQ address to connect to.
+                       ( e.g.: tcp://127.0.0.1:1234 )
+                       If not specified, the one in settings.py
+                       is used instead.
+    [-h|--help]      - Show this message and exit.
+
+"""
+
 from autobahn.asyncio.websocket import WebSocketServerProtocol, WebSocketServerFactory
 import asyncio
 import backend
 import threading
 import json
 import sys
+
+import settings
 
 
 class GraphProtocol(WebSocketServerProtocol):
@@ -12,13 +26,23 @@ class GraphProtocol(WebSocketServerProtocol):
     }
 
     def onConnect(self, request):
-        print("Client connecting: {0}".format(request.peer))
+        """Shows the address of the client connecting"""
+        print('Client connecting: {0}'.format(request.peer))
 
     def onMessage(self, payload, isBinary):
+        """When receiving a message, compare it against our list of
+           valid messages, then if it's there, call the function therein.
+           Otherwise just print the message"""
         if isBinary: return
         self.valid_messages.get(payload, print)()
 
     def __init__(self):
+        """
+        Instantiates the protocol, and binds sending the serialized JSON data
+        to the following observables:
+         - chart_dict
+         - path_dict
+        """
         def _process_value(value):
             self.sendMessage(json.dumps(value).encode('utf-8'), isBinary=False)
 
@@ -29,6 +53,11 @@ class GraphProtocol(WebSocketServerProtocol):
 
 
 def main(*args):
+    """
+    Starts the autobahn websockets server, and the receive loop for zeroMQ.
+    Defaults for the ZeroMQ address (also overridable via commandline) and the
+    websockets server can be overridden in the settings module.
+    """
     receive_loop = threading.Thread(target=backend.receive_loop, args=args)
     receive_loop.start()
 
@@ -36,10 +65,11 @@ def main(*args):
     factory.protocol = GraphProtocol
 
     loop = asyncio.get_event_loop()
-    coro = loop.create_server(factory, '127.0.0.1', 9000)
+    coro = loop.create_server(factory, *settings.WEBSOCKETS_ADDRESS)
     server = loop.run_until_complete(coro)
 
     try:
+        print('Starting the websockets server on %s:%d' % settings.WEBSOCKETS_ADDRESS)
         loop.run_forever()
     except KeyboardInterrupt:
         pass
@@ -48,5 +78,9 @@ def main(*args):
         server.close()
         loop.close()
         sys.exit(0)
+
 if __name__ == '__main__':
+    if any([x.lower() in ('-h', '--help') for x in sys.argv[1:]]):
+        print(__doc__)
+        sys.exit()
     main(*sys.argv[1:])
