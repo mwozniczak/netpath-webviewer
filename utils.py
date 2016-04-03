38df4fcd8f4471e6624372e0406a8a06f8ba72dd
Michał Woźniczak <michal.wozniczak@gmail.com>
2016-04-03 13:46:08 +0200

    Trimmed out unused periodic update functionality

--- utils.py
+++ utils.py
@@ -1,7 +1,5 @@
 from itertools import tee
 from threading import RLock
-# import settings
-# from datetime import datetime, timedelta
 
 
 def pairwise(iterable):
@@ -24,8 +22,6 @@ class ObservableValue():
         self.value = original_value
         self.observers = []
         self.lock = RLock()
-        # self.last_emit = datetime.now()
-        # self.emit_every = timedelta(**settings.REBROADCAST_INTERVAL)
 
     def add_observers(self, *observers):
         """
@@ -40,7 +36,6 @@ class ObservableValue():
         """
         for observer in self.observers:
             observer(self.value)
-        # self.last_emit = datetime.now()
 
     def overwrite(self, newval):
         """
@@ -50,8 +45,6 @@ class ObservableValue():
         """
         with self.lock:
             if newval == self.value:
-                # if datetime.now() - self.last_emit >= self.emit_every:
-                #     self.emit_value()
                 return
             self.value = newval
             self.emit_value()
